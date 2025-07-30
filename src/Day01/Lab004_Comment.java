ee12fd9f312aecf52036f0e7b70e77d35720d9ad
Ashish <elx.ashish@gmail.com>
2025-07-30 10:25:16 +0530

    first Commit

---
+++ src/Day01/Lab004_Comment.java
@@ -0,0 +1,8 @@
+package Day01;
+
+public class Lab004_Comment {
+    public static void main(String[] args) {
+        // This is a Comment, It will be ignored by JVM/ Compiler
+        System.out.println("Hi");
+    }
+}
