3c8e5d266a9cbf7b215a3c360b384be3a7535a70
Ashish <elx.ashish@gmail.com>
2025-07-30 10:25:16 +0530

    second Commit

---
+++ src/Day01/Lab006_Comments_Multi.java
@@ -0,0 +1,12 @@
+package Day01;
+
+public class Lab006_Comments_Multi {
+    public static void main(String[] args) {
+        /**
+         * Author : Ashish
+         * Course :  ATB2x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+}
