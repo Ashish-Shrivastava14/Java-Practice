c9074a85d5f7f17f2de9dbbf75689211c1616a06
Ashish <elx.ashish@gmail.com>
2025-07-30 10:25:16 +0530

    first Commit

---
+++ src/Day01/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,11 @@
+package Day01;
+
+public class Lab007_Comments_ShortCuts {
+    public static void main(String[] args) {
+        // This code will print the Hi
+        // Jr QA - this will print the hi in console
+
+
+        System.out.println("Hi");
+    }
+}
---
+++ src/Day01/Lab008_Comment.java
@@ -0,0 +1,14 @@
+package Day01;
+
+public class Lab008_Comment {
+    /**
+     * Author - Ashish
+     * Course :  ATB 2x
+     * This is a documentation comment
+     * @param args command-line arguments
+     * @return void
+     */
+    public static void main(String[] args) {
+        // Method implementation
+    }
+}
